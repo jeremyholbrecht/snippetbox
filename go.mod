0e196ff6a88cf91b0109cf14916a2329d1ecea25
jeremyholbrecht <griffley@proton.me>
2024-08-16 12:48:14 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeremyholbrecht/snippetbox
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "Hello, Welcome to Snippetbox!")
+
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Display a specific snippet...")
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Create a new snippet...")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Print("Starting server on :8080...")
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
