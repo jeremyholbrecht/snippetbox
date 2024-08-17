b0a23098c77b94c7b722cdee576ce85ad73683a0
jeremyholbrecht <griffley@proton.me>
2024-08-17 20:50:43 +0200

    set snippetCreate to only allow POST requests

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	fmt.Fprint(w, "Create a new snippet...")
 }
 
